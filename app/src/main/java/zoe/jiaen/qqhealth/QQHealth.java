013d22e0de7ac79d931718dd1e2ed31d8f0eebe0
Liu <liujilong.me@gmail.com>
2015-12-15 16:35:41 +0800

    Initial commit

---
+++ app/src/main/java/zoe/jiaen/qqhealth/QQHealth.java
@@ -0,0 +1,13 @@
+package zoe.jiaen.qqhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QQHealth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qqhealth);
+    }
+}
